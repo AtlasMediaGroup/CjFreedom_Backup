a244191eb16f59a2e9dc2b7e93559459d2c349b2
TheCJGCJG <charlesgillham@me.com>
2013-04-16 11:36:20 +0100

    Updated

--- autobackup.php
+++ autobackup.php
@@ -1,14 +1,18 @@
 <?php
 $password = 'httpdpassword';
+//Enter Your Bukkit HttpD password Here
 $address = 'serveraddress:port';
+//Enter Your HttpD Address Here E.G. http://8.8.8.8:12345
 
 
 $whitelist = array('94.168.98.195', '109.70.149.212');
+// Add IP's That Are Allowed To Run The Backup To This Location
 if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
     include 'backup/backup.php';
 } else {
     echo 'Sorry This Webpage Can Only Be Accessed By The CjFreedom Server @ mc.thecjgcjg.com'; 
     echo "<br />IP Address: ".$_SERVER['REMOTE_ADDR'];
+	//Enter Your Not Authorised Message
     exit;
 }
 ?>
