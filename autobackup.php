6d8662783328bdc6e0e0870a3e85c4760a1c241a
TheCJGCJG <charlesgillham@me.com>
2013-04-16 11:47:47 +0100

    Added Credit

--- autobackup.php
+++ autobackup.php
@@ -9,6 +9,7 @@ $whitelist = array('94.168.98.195', '109.70.149.212');
 // Add IP's That Are Allowed To Run The Backup To This Location
 if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
     include 'backup/backup.php';
+	echo '<!-- By TheCJGCJG https://github.com/TheCJGCJG/CjFreedom_Backup -->';
 } else {
     echo 'Sorry This Webpage Can Only Be Accessed By The CjFreedom Server @ mc.thecjgcjg.com'; 
     echo "<br />IP Address: ".$_SERVER['REMOTE_ADDR'];
