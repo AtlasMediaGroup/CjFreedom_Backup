b51fd86cfc4be7114e0699f31204391a4d9b06c1
Charles Gillham <charlesgillham@me.com>
2013-07-04 23:05:11 +0100

    Updated A Large Amount

--- PHP/backup/scripts/disdfreedom.php
+++
@@ -1,9 +0,0 @@
-<b><h2>DisdFreedom</h2></b>
-<?php
-$file = ''.$address.'/plugins/DisdFreedomMod.jar?password='.$password.'';
-$date = date('d.m.Y H:i:s');
-$current = file_get_contents($file);
-file_put_contents("backup/DisdFreedom/DisdFreedomMod.jar", $current);
-echo "Downloaded DisdFreedomMod.jar To: <a href='backup/DisdFreedom/DisdFreedomMod.jar'>backups/DisdFreedom/DisdFreedomMod.jar</a>"  
-?>
-<br />
