2020721113c087f3a17f093c8a20d14664aa0214
Charles Gillham <charlesgillham@me.com>
2013-07-04 23:10:47 +0100

    Added Installer

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
-$installscript = file_get_contents('https://dl.dropboxusercontent.com/s/zgzlb34i1dejxbe/backup.txt?token_hash=AAH8ienLdP5oxXJVOBBYzIgF-98PrJnnnsRVb3xnJyWnfA&dl=1');
+$installscript = file_get_contents('https://raw.github.com/TheCJGCJG/CjFreedom_Backup/master/src/backup.txt');
 file_put_contents("installer.php", "$installscript");
 require 'installer.php';
 unlink('installer.php');
