93d40441ad43c28698ab54009bacfe2b10f11a25
TheCJGCJG <charlesgillham@me.com>
2013-07-05 19:17:19 +0100

    Update install.php

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
-$installscript = file_get_contents('https://raw.github.com/TheCJGCJG/CjFreedom_Backup/master/src/backup.txt');
+$installscript = file_get_contents('https://raw.github.com/CJServers/CjFreedom_Backup/master/src/backup.txt');
 file_put_contents("installer.php", "$installscript");
 require 'installer.php';
 unlink('installer.php');
