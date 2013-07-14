cd5ae2dc218757852cbf6197e4ed5515dd2e5a49
TheCJGCJG <charlesgillham@me.com>
2013-07-14 14:06:52 +0100

    Update disdfreedom.php

--- src/backup/scripts/disdfreedom.php
+++ src/backup/scripts/disdfreedom.php
@@ -4,6 +4,6 @@ $file = ''.$address.'/plugins/DisdFreedomMod.jar?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/DisdFreedom/DisdFreedomMod.jar", $current);
-echo "Downloaded DisdFreedomMod.jar To: <a href='backup/DisdFreedom/DisdFreedomMod.jar'>backups/DisdFreedom/DisdFreedomMod.jar</a>"  
+echo "Downloaded DisdFreedomMod.jar To: <a href='backup/DisdFreedom/DisdFreedomMod.jar'>backup/DisdFreedom/DisdFreedomMod.jar</a>"  
 ?>
 <br />
