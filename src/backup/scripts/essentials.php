e889f7f1840d9206cb62fc75f0540476291d716b
TheCJGCJG <charlesgillham@me.com>
2013-07-14 14:07:33 +0100

    Update essentials.php
    Updated

--- src/backup/scripts/essentials.php
+++ src/backup/scripts/essentials.php
@@ -5,7 +5,7 @@ $file = ''.$address.'/plugins/Essentials.jar?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/Essentials/Essentials/Essentials.jar", $current);
-echo "Downloaded Essentials.jar To: <a href='backup/Essentials/Essentials.jar'>backups/Essentials/Essentials.jar</a>" 
+echo "Downloaded Essentials.jar To: <a href='backup/Essentials/Essentials.jar'>backup/Essentials/Essentials.jar</a>" 
 ?>
 <br />
 <?php
@@ -13,7 +13,7 @@ $file = ''.$address.'/plugins/EssentialsSpawn.jar?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/Essentials/Essentials/EssentialsSpawn.jar", $current);
-echo "Downloaded EssentialsSpawn.jar To: <a href='backup/Essentials/EssentialsSpawn.jar'>backups/Essentials/EssentialsSpawn.jar</a>" 
+echo "Downloaded EssentialsSpawn.jar To: <a href='backup/Essentials/EssentialsSpawn.jar'>backup/Essentials/EssentialsSpawn.jar</a>" 
 ?>
 <br />
 <?php
@@ -21,7 +21,7 @@ $file = ''.$address.'/plugins/Essentials/config.yml?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/Essentials/Essentials/config.yml", $current);
-echo "Downloaded config.yml To: <a href='backup/Essentials/config.yml'>Essentials/Essentials/config.yml</a>" 
+echo "Downloaded config.yml To: <a href='backup/Essentials/config.yml'>backup//Essentials/config.yml</a>" 
 ?>
 <br />
 <?php
@@ -29,7 +29,7 @@ $file = ''.$address.'/plugins/Essentials/motd.txt?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/Essentials/Essentials/motd.txt", $current);
-echo "Downloaded motd.txt To: <a href='backup/Essentials/motd.txt'>backups/Essentials/motd.txt</a>" 
+echo "Downloaded motd.txt To: <a href='backup/Essentials/motd.txt'>backup/Essentials/motd.txt</a>" 
 ?>
 <br />
 <?php
@@ -37,6 +37,6 @@ $file = ''.$address.'/plugins/Essentials/config.yml?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/Essentials/Essentials/config.yml", $current);
-echo "Downloaded config.yml To: <a href='backup/Essentials/config.yml'>Essentials/config.yml</a>" 
+echo "Downloaded config.yml To: <a href='backup/Essentials/config.yml'>backup/Essentials/config.yml</a>" 
 ?>
 <br />
