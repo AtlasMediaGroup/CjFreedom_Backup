89aa40b84d7d4d8df2a507c10d140ac9c99466c7
TheCJGCJG <charlesgillham@me.com>
2013-07-14 14:06:38 +0100

    Update bukkittelnet.php

--- src/backup/scripts/bukkittelnet.php
+++ src/backup/scripts/bukkittelnet.php
@@ -5,7 +5,7 @@ $file = ''.$address.'/plugins/BukkitTelnet.jar?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/BukkitTelnet/BukkitTelnet.jar", $current);
-echo "Downloaded BukkitTelnet.jar To: <a href='backup/BukkitTelnet/BukkitTelnet.jar'>backups/BukkitTelnet/BukkitTelnet.jar</a>" 
+echo "Downloaded BukkitTelnet.jar To: <a href='backup/BukkitTelnet/BukkitTelnet.jar'>backup/BukkitTelnet/BukkitTelnet.jar</a>" 
 ?>
 <br />
 <?php
@@ -13,6 +13,6 @@ $file = ''.$address.'/plugins/BukkitTelnet/config.yml?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/BukkitTelnet/config.yml", $current);
-echo "Downloaded config.yml To: <a href='backup/BukkitTelnet/config.yml'>backups/BukkitTelnet/config.yml</a>" 
+echo "Downloaded config.yml To: <a href='backup/BukkitTelnet/config.yml'>backup/BukkitTelnet/config.yml</a>" 
 ?>
 <br />
