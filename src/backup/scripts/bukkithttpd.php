39c88e4e3d918b915bb8393d5870a056d552beb0
TheCJGCJG <charlesgillham@me.com>
2013-07-14 14:06:19 +0100

    Update bukkithttpd.php
    Fixed

--- src/backup/scripts/bukkithttpd.php
+++ src/backup/scripts/bukkithttpd.php
@@ -4,7 +4,7 @@ $file = ''.$address.'/plugins/BukkitHttpd.jar?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/BukkitHttpd/BukkitHttpd.jar", $current);
-echo "Downloaded BukkitHttpd.jar To: <a href='backup/BukkitHttpd/BukkitHttpd.jar'>backups/BukkitHttpd/BukkitHttpd.jar</a>" 
+echo "Downloaded BukkitHttpd.jar To: <a href='backup/BukkitHttpd/BukkitHttpd.jar'>backup/BukkitHttpd/BukkitHttpd.jar</a>" 
 ?>
 <br />
 <?php
@@ -12,6 +12,6 @@ $file = ''.$address.'/plugins/BukkitHttpd/config.yml?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
 file_put_contents("backup/BukkitHttpd/config.yml", $current);
-echo "Downloaded config.yml To: <a href='backup/BukkitHttpd/config.yml'>backups/BukkitHttpd/config.yml</a>" 
+echo "Downloaded config.yml To: <a href='backup/BukkitHttpd/config.yml'>backup/BukkitHttpd/config.yml</a>" 
 ?>
 <br />
