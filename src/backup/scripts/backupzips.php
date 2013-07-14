4035695297ef735ccfad726c6e1e8a53d06e4944
TheCJGCJG <charlesgillham@me.com>
2013-07-14 14:04:58 +0100

    Update backupzips.php
    Fixed

--- src/backup/scripts/backupzips.php
+++ src/backup/scripts/backupzips.php
@@ -2,39 +2,39 @@
 $file = ''.$address.'/backups-BukkitTelnet.zip?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
-file_put_contents("backup/BukkitTelnet/backups.zip", $current);
-echo "Downloaded /BukkitTelnet To: <a href='backupss/BukkitTelnet/backups.zip'>backupss/BukkitTelnet/backups.zip</a>" 
+file_put_contents("backup/BukkitTelnet/BukkitTelnet.zip", $current);
+echo "Downloaded /BukkitTelnet To: <a href='backup/BukkitTelnet/BukkitTelnet.zip'>backup/BukkitTelnet/BukkitTelnet.zip</a>" 
 ?>
 <br />
 <?php
 $file = ''.$address.'/backups-Essentials.zip?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
-file_put_contents("backup/Essentials/backups.zip", $current);
-echo "Downloaded /Essentials To: <a href='backupss/Essentials/backups.zip'>backupss/Essentials/backups.zip</a>" 
+file_put_contents("backup/Essentials/Essentials.zip", $current);
+echo "Downloaded /Essentials To: <a href='backup/Essentials/Essentials.zip'>backup/Essentials/Essentials.zip</a>" 
 ?>
 <br />
 <?php
 $file = ''.$address.'/backups-plugins.zip?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
-file_put_contents("backup/backups.zip", $current);
-echo "Downloaded /plugins To: <a href='backupss/backups.zip'>backupss/backups.zip</a>" 
+file_put_contents("backup/plugins.zip", $current);
+echo "Downloaded /plugins To: <a href='backup/plugins.zip'>backup/plugins.zip</a>" 
 ?>
 <br />
 <?php
 $file = ''.$address.'/backups-TotalFreedomMod.zip?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
-file_put_contents("backup/TotalFreedomMod/backups.zip", $current);
-echo "Downloaded /TotalFreedomMod To: <a href='backupss/TotalFreedomMod/backups.zip'>backupss/TotalFreedomMod/backups.zip</a>" 
+file_put_contents("backup/TotalFreedomMod/TotalFreedomMod.zip", $current);
+echo "Downloaded /TotalFreedomMod To: <a href='backup/TotalFreedomMod/TotalFreedomMod.zip'>backup/TotalFreedomMod/TotalFreedomMod.zip</a>" 
 ?>
 <br />
 <?php
 $file = ''.$address.'/backups-WorldEdit.zip?password='.$password.'';
 $date = date('d.m.Y H:i:s');
 $current = file_get_contents($file);
-file_put_contents("backup/WorldEdit/backups.zip", $current);
-echo "Downloaded /Worldedit To: <a href='backupss/WorldEdit/backups.zip'>backupss/WorldEdit/backups.zip</a>" 
+file_put_contents("backup/WorldEdit/WorldEdit.zip", $current);
+echo "Downloaded /Worldedit To: <a href='backup/WorldEdit/WorldEdit.zip'>backup/WorldEdit/WorldEdit.zip</a>" 
 ?>
 <br />
